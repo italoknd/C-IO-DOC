290899bcfeb56f7bfba85ef725bf4ffb7abc68ce
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2022-02-24 02:18:22 -0400

    inicio do formulário

--- controle_docs/src/components/Agendar.vue
+++ controle_docs/src/components/Agendar.vue
@@ -1,14 +1,49 @@
 <template>
-  <div>
-    <h1>aqui voce realiza agendamentos</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ullam harum ipsa dignissimos, voluptatibus possimus quaerat aliquam, nihil quas distinctio nulla delectus ea explicabo dicta? Est modi harum nihil culpa.</p>
+  <div id="component">
+    <form action="">
+      <div class="row">
+        <div class="column">
+          <div class="row">
+            <div class="input-field">
+              <label for="completeName">Nome Completo: </label>
+              <input
+                id="completeName"
+                type="text"
+                placeholder="Informe o nome aqui"
+              />
+            </div>
+            <div class="input-field">
+              <label for="CPF">CPF: </label>
+              <input id="CPF" type="tel" placeholder="Informe o CPF aqui" />
+            </div>
+          </div>
+          <div class="input-field">
+            <label for="tel">Telefone: </label>
+            <input id="tel" type="tel" placeholder="Informe o Telefone aqui" />
+          </div>
+          <div id="doctype">
+            <label for="doctype">Selecione o tipo de documento: </label>
+            <select name="doctype" id="doctype">
+              <option value="" selected disabled>Escolha um</option>
+              <option value="">RG</option>
+              <option value="">CPF</option>
+              <option value="">Espelho</option>
+            </select>
+          </div>
+          <div id="date-input">
+            <label for="">Data de entrega estimada: </label>
+            <input type="date" name="" id="" />
+          </div>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
-  export default{
-    name: "Agendar",
-  }
+export default {
+  name: 'Agendar'
+}
 </script>
 
 <style scoped>
